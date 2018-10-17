b1dbeef64e94c6f4eabdd6786938ab9592bfe21d
shuyj <shu.yajun@immomo.com>
2018-10-17 17:04:20 +0800

    测试lambda与对象生命周期

--- devbase/cppdev/main.cpp
+++ devbase/cppdev/main.cpp
@@ -7,5 +7,48 @@
 //
 
 #include <iostream>
+#include "MThreadPool.hpp"
 
+class A : public std::enable_shared_from_this<A> {
+    std::string  mcontent = "A content";
+    char   mc2[20] = "";
+public:
+    ~A(){
+        std::cout<< "~A destroy" << std::endl;
+    }
+    void printtest(){
+        mcontent = "0";
+        mc2[0] = '1';
+        std::cout<< mcontent << mc2 << std::endl;
+    }
+    
+    void println(){
+        std::cout<< "A print" << std::endl;
+        std::weak_ptr<A> weak_this = shared_from_this();
+        MThreadPool::instance()->async([&, weak_this](){
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            auto that = weak_this.lock();
+            if (!that) {
+                std::cout << "The object is already dead" << std::endl;
+                return;
+            }
+            for (int i=0; i<5; i++) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                printtest();
+            }
+        });
+    }
+};
 
+int main(int argc, const char * argv[]) {
+    
+    MThreadPool::Create(1);
+    
+    std::shared_ptr<A> a = std::make_shared<A>();
+    std::cout<< "A print111" << std::endl;
+    a->println();
+    std::cout<< "A print222" << std::endl;
+    a.reset();
+    std::cout<< "A print333" << std::endl;
+    MThreadPool::Destroy();
+}
